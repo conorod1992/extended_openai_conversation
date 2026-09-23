752da28ad88ee57fe02e55c0401c8bd5f46b0731
conorod1992 <58264595+conorod1992@users.noreply.github.com>
2026-09-23 01:06:40 +0100

    Load Function Tool editors and HA catalogue interactions on demand

---
+++ custom_components/extended_openai_conversation_responses/frontend/dist/assets/management-capabilities-ia-BgOd-YyE.js
@@ -0,0 +1,1 @@
+import{u as e}from"./management-core-DqSyPPOg.js";import{i as t,n,r,t as i}from"./management-capabilities-ia-CiKhoPIW.js";export{i as bindCapabilities,n as knowledgeAvailabilityMarkup,r as knowledgeSourceAvailabilityBadge,e as knowledgeSourceAvailabilityControl,t as renderConfiguration};
